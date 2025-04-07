d7a445460f32022a1e1be289b33712673569b5b3
solvhold <solvhold@stud.ntnu.no>
2025-04-07 17:11:29 +0200

    fix: merge fix
    vf-247

--- db/seeding/seeding-tables.ts
+++ db/seeding/seeding-tables.ts
@@ -1,7 +1,7 @@
+import { teamApplicationsTable } from "@/db/tables/applications";
 import { departmentsTable } from "@/db/tables/departments";
 import { expensesTable } from "@/db/tables/expenses";
 import { fieldsOfStudyTable } from "@/db/tables/fields-of-study";
-import { teamApplicationsTable } from "@/db/tables/team-applications";
 import { teamsTable } from "@/db/tables/teams";
 import { usersTable } from "@/db/tables/users";
 
