fced5c0c6ae9da9f413a0c0407eb559618a50ef5
TobiasH05 <tobiashegge05@gmail.com>
2025-04-10 10:56:37 +0200

    fix: :bug: fixed semester relations

--- db/tables/semesters.ts
+++ db/tables/semesters.ts
@@ -1,39 +1,40 @@
 import { relations } from "drizzle-orm";
-import { date, foreignKey, integer, serial, text } from "drizzle-orm/pg-core";
+import {
+	type AnyPgColumn,
+	date,
+	integer,
+	serial,
+	text,
+} from "drizzle-orm/pg-core";
 import { departmentsTable } from "./departments";
 import { mainSchema } from "./schema";
 
-export const semestersTable = mainSchema.table(
-	"semesters",
-	{
-		id: serial("id").primaryKey(),
-		lastSemesterId: integer("lastSemesterId"),
-		semesterStartDate: date("semesterStartDate").notNull(),
-		semesterEndDate: date("semesterEndDate").notNull(),
-		recruitmentStartDate: date("recruitmentStartDate").notNull(),
-		recruitmentEndDate: date("recruitmentEndDate").notNull(),
-		departmentId: integer("departmentId")
-			.notNull()
-			.references(() => departmentsTable.id),
-		name: text("name").notNull(),
-	},
-	(table) => {
-		return {
-			parentReference: foreignKey({
-				columns: [table.departmentId],
-				foreignColumns: [table.id],
-			}),
-		};
-	},
-);
+export const semestersTable = mainSchema.table("semesters", {
+	id: serial("id").primaryKey(),
+	lastSemesterId: integer("lastSemesterId").references(
+		(): AnyPgColumn => semestersTable.id,
+	),
+	semesterStartDate: date("semesterStartDate").notNull(),
+	semesterEndDate: date("semesterEndDate").notNull(),
+	recruitmentStartDate: date("recruitmentStartDate").notNull(),
+	recruitmentEndDate: date("recruitmentEndDate").notNull(),
+	departmentId: integer("departmentId")
+		.notNull()
+		.references(() => departmentsTable.id),
+	name: text("name").notNull(),
+});
 
 export const semestersRelations = relations(semestersTable, ({ one }) => ({
 	department: one(departmentsTable, {
 		fields: [semestersTable.departmentId],
 		references: [departmentsTable.id],
 	}),
-	semester: one(semestersTable, {
+	lastSemester: one(semestersTable, {
 		fields: [semestersTable.lastSemesterId],
 		references: [semestersTable.id],
+	}),
+	nextSemester: one(semestersTable, {
+		fields: [semestersTable.id],
+		references: [semestersTable.lastSemesterId],
 	}),
 }));
