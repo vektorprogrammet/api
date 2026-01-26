743b92485621aaf92c60d3e6d4d0095ba63d742d
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-01-26 18:22:10 +0100

    added relations for assistant and semester in schoolassignments table

--- db/tables/school-assignments.ts
+++ db/tables/school-assignments.ts
@@ -2,6 +2,8 @@ import { relations } from "drizzle-orm";
 import { date, foreignKey, integer, serial, time } from "drizzle-orm/pg-core";
 import { assistantSemestersTable } from "./assistant-semesters";
 import { mainSchema } from "./schema";
+import { semestersTable } from "./semesters";
+import { assistantUsersTable } from "./users";
 
 export const schoolAssignmentsTable = mainSchema.table(
 	"schoolAssignments",
@@ -28,5 +30,13 @@ export const schoolAssignmentsRelations = relations(
 	schoolAssignmentsTable,
 	({ one }) => ({
 		assistantSemester: one(assistantSemestersTable),
+		assistant: one(assistantUsersTable, {
+			fields: [schoolAssignmentsTable.assistantId],
+			references: [assistantUsersTable.id],
+		}),
+		semester: one(semestersTable, {
+			fields: [schoolAssignmentsTable.semesterId],
+			references: [semestersTable.id],
+		}),
 	}),
 );
