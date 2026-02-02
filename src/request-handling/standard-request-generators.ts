6121548a216f14c379049b74462d92ef7f0c5411
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2026-02-02 18:56:02 +0100

    Made plural api function calls for the most standard category of calls

---
+++ src/request-handling/standard-request-generators.ts
@@ -0,0 +1,91 @@
+import type { RequestHandler } from "express";
+import type { ZodSchema } from "zod";
+import { clientError } from "./error/http-errors";
+import type { OrmResult } from "./error/orm-error";
+import { QueryParameters } from "./request-handling/common";
+
+type DatabaseCall<Req, Res> = (items: Req) => Promise<OrmResult<Res>>;
+
+function makeStandardBodyRequestHandler<Req, Res>(
+	zodSchema: ZodSchema<Req>,
+	databaseCall: DatabaseCall<Req[], Res>,
+	options: { httpReturnCode: number } = { httpReturnCode: 200 },
+): RequestHandler {
+	return async (req, res, next) => {
+		const requestResult = zodSchema.safeParse(req.body);
+		if (!requestResult.success) {
+			const error = clientError(
+				400,
+				"Invalid request format",
+				requestResult.error,
+			);
+			return next(error);
+		}
+		const databaseResult = await databaseCall([requestResult.data]);
+		if (!databaseResult.success) {
+			const error = clientError(
+				400,
+				"Failed to execute the database command",
+				databaseResult.error,
+			);
+			return next(error);
+		}
+		res.status(options.httpReturnCode).json(databaseResult.data);
+	};
+}
+
+function makeStandardQueryRequestHandler<Res>(
+	zodSchema: ZodSchema<QueryParameters>,
+	databaseCall: DatabaseCall<QueryParameters, Res>,
+	options: { httpReturnCode: number } = { httpReturnCode: 200 },
+): RequestHandler {
+	return async (req, res, next) => {
+		const requestResult = zodSchema.safeParse(req.query);
+		if (!requestResult.success) {
+			const error = clientError(
+				400,
+				"Invalid request format",
+				requestResult.error,
+			);
+			return next(error);
+		}
+		const databaseResult = await databaseCall(requestResult.data);
+		if (!databaseResult.success) {
+			const error = clientError(
+				400,
+				"Failed to execute the database command",
+				databaseResult.error,
+			);
+			return next(error);
+		}
+		res.status(options.httpReturnCode).json(databaseResult.data);
+	};
+}
+
+function makeStandardParameterHandler<Param, Res>(
+	zodSchema: ZodSchema<Param>,
+	databaseCall: DatabaseCall<Param, Res>,
+	options: { httpReturnCode: number } = { httpReturnCode: 200 },
+): RequestHandler {
+	return async (req, res, next) => {
+		const requestResult = zodSchema.safeParse(req.params);
+		if (!requestResult.success) {
+			const error = clientError(
+				400,
+				"Invalid request format",
+				requestResult.error,
+			);
+			return next(error);
+		}
+		const databaseResult = await databaseCall(requestResult.data);
+		if (!databaseResult.success) {
+			const error = clientError(
+				400,
+				"Failed to execute the database command",
+				databaseResult.error,
+			);
+			return next(error);
+		}
+		res.status(options.httpReturnCode).json(databaseResult.data);
+	};
+}
