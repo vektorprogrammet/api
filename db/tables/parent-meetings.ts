50fbe687b32ed9824b4a9de2f6d5dfd095c57f02
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-02-05 17:28:55 +0100

    check

--- db/tables/parent-meetings.ts
+++ db/tables/parent-meetings.ts
@@ -1,5 +1,11 @@
 import { relations } from "drizzle-orm";
-import { integer, interval, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	integer,
+	interval,
+	serial,
+	text,
+	timestamp,
+} from "drizzle-orm/pg-core";
 import { mainSchema } from "./schema";
 import { semestersTable } from "./semesters";
 import { teamUsersTable } from "./users";
